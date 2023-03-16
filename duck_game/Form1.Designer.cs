93d28427fbefd79dba6d012702eecf1629ebb55f
LAPTOP-IPS775OC\Acer <damdud4776@gmail.com>
2023-03-16 14:00:07 +0100

    Working game

---
+++ duck_game/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace zad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(248, 95);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(268, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(339, 204);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 29);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(243, 206);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "NICK:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.trackBar1.LargeChange = 2;
+            this.trackBar1.Location = new System.Drawing.Point(12, 393);
+            this.trackBar1.Maximum = 7;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(151, 45);
+            this.trackBar1.SmallChange = 2;
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Visible = false;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "EASY";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(112, 374);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "HARD";
+            this.label3.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::zad2.Properties.Resources.duck_ikon;
+            this.button2.Location = new System.Drawing.Point(369, 116);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(33, 45);
+            this.button2.TabIndex = 6;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.Location = new System.Drawing.Point(323, 391);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(160, 47);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Exit and go to Menu";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Image = global::zad2.Properties.Resources.duck1;
+            this.label4.Location = new System.Drawing.Point(641, 366);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "SCORE:";
+            this.label4.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(634, 393);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(94, 45);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 4000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::zad2.Properties.Resources.duck1;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ duck_game/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace zad2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string nick;
+        private int score = 1;
+        private int gscore = 0;
+        private int zm = 1;
+        private int czas = 0;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nick = textBox1.Text;
+            button1.Visible = false;
+            textBox1.Visible = false;
+            label1.Visible = false;
+            this.BackgroundImage = Properties.Resources.duck2;
+            label2.Visible = true;
+            label3.Visible = true;
+            trackBar1.Visible = true;
+            // timer1.Enabled = true;
+            button2.Visible = true;
+            button3.Visible = true;
+            label4.Visible = true;
+            textBox2.Visible = true;
+            this.Cursor = Cursors.Cross;
+
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gscore++;
+            button2.Visible = false;
+            Point loc = button2.Location;
+            loc.X = 0;
+            loc.Y = 0;
+            button2.Location = loc;
+            Random r = new Random();
+            int zm2 = 10;
+            
+            czas = timer1.Interval - (zm2 + r.Next(10, 50));
+            if (czas > 1) timer1.Interval = czas;
+
+            timer1.Stop();
+            timer1.Start();
+            score_res();
+
+
+
+
+
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            score = 1;
+            gscore = 0;
+            timer1.Stop();
+            timer2.Stop();
+            zm = 4000/trackBar1.Value;
+            timer2.Interval = zm;
+            button2.Visible = true;
+            Point loc = button2.Location;
+            loc.X = 369;
+            loc.Y = 116;
+            button2.Location = loc;
+            score_res();
+
+        }
+
+        public void game()
+        {
+            timer2.Stop();
+            timer2.Start();
+
+            Random r = new Random();
+            Point loc = button2.Location;
+            
+            loc.X = 50+r.Next(0, 650);
+            loc.Y = 25+r.Next(0, 300);
+            button2.Location = loc;
+            button2.Visible = true;
+            score++;
+            score_res();
+
+
+        }
+
+
+        private void score_res()
+        {
+
+            textBox2.Text = " " + Convert.ToString(gscore) + " / " + Convert.ToString(score) + " ";
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            game();
+            timer1.Stop();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            timer1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer2.Stop();
+            timer1.Stop();
+            button1.Visible = true;
+            textBox1.Visible = true;
+            label1.Visible = true;
+            this.BackgroundImage = Properties.Resources.duck1;
+            label2.Visible = false;
+            label3.Visible = false;
+            trackBar1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            label4.Visible = false;
+            textBox2.Visible = false;
+            this.Cursor = Cursors.Arrow;
+            trackBar1.Value = 1;
+            zm = 1;
+            Point loc = button2.Location;
+            loc.X = 369;
+            loc.Y = 116;
+            button2.Location = loc;
+            using (StreamWriter sw = new StreamWriter("sboard.txt", true))
+            {
+                sw.WriteLine(nick+" " + Convert.ToString(gscore) + " / " + Convert.ToString(score-1) + "\r\n");
+            }
+            score = 1;
+            gscore = 0;
+        }
+    }
+}
